7c35157b26cb06df0874ec0db3fb8156ec2d960a
Eslam <eslamesw@gmail.com>
2020-11-26 15:07:40 +0200

    common details

--- app/src/main/java/com/semi/awlem/ui/home/menu/help/HelpViewModel.kt
+++ app/src/main/java/com/semi/awlem/ui/home/menu/help/HelpViewModel.kt
@@ -3,29 +3,35 @@ package com.semi.awlem.ui.home.menu.help
 import android.os.Bundle
 import android.view.View
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.google.android.material.textfield.TextInputEditText
 import com.semi.awlem.R
 import com.semi.awlem.base.BaseViewModel
 import com.semi.awlem.utility.ContextConverter.getActivity
+import com.semi.awlem.utility.KeyboardUtil.hideKeyboard
 import com.semi.awlem.utility.NavigationUtil.findNavigationController
 import com.semi.awlem.utility.NavigationUtil.navigateTo
 import com.semi.awlem.utility.SnackBar.customSnackBar
+import com.semi.awlem.utility.isInputEmpty
+import com.semi.awlem.utility.showErrorSnackBar
 import com.semi.entity.response.menu.FaqResponse
-import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class HelpViewModel @ViewModelInject constructor(
     private val repository: HelpRepository
 ) : BaseViewModel(), FaqsAdapter.ClickListener {
     val faqsAdapter = FaqsAdapter(this)
+    val messageAddress = MutableLiveData<String>("")
+    val content = MutableLiveData<String>("")
 
     init {
         getFaq(null)
     }
 
     fun getFaq(v: View?) {
-        viewModelScope.async {
+        viewModelScope.launch {
             repository.getFaqTaskRepo(onLoading = { loading: Boolean ->
-                _isLoading.value = loading
             },
                 onSuccess = { list: List<FaqResponse>? ->
                     faqsAdapter.submitList(list)
@@ -38,6 +44,40 @@ class HelpViewModel @ViewModelInject constructor(
                         icon,
                     ) {}
                 })
+        }
+    }
+
+    fun loginClick(
+        v: View,
+        phoneTextInput: TextInputEditText,
+        passwordTextInput: TextInputEditText
+    ) {
+        val activity = v.context.getActivity()
+
+        val messageAddressValue = messageAddress.value
+        val contentValue = content.value
+        when {
+            isInputEmpty(messageAddressValue) -> showErrorSnackBar(
+                activity = activity,
+                editText = phoneTextInput,
+                errorTitleId = R.string.phoneReq,
+                errorMessageId = R.string.phoneReq,
+                errorIconId = R.drawable.ic_phone_error
+            )
+            isInputEmpty(contentValue) -> showErrorSnackBar(
+                activity = activity,
+                editText = passwordTextInput,
+                errorTitleId = R.string.PasswordReq,
+                errorMessageId = R.string.PasswordReq,
+                errorIconId = R.drawable.ic_password_error
+            )
+
+            else -> {
+                v.hideKeyboard()
+                viewModelScope.launch {
+
+                }
+            }
         }
     }
 
